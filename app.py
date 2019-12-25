beee0edc319e0eecaeff6c9c407e9e37c1055829
samip584 <samip584@gmail.com>
2019-12-25 13:43:19 +0545

    traffic db and driver db

--- app.py
+++ app.py
@@ -19,40 +19,69 @@ app.config['SECRET_KEY'] = 'buzzgopa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socket = SocketIO(app)
-# Init db
-db = SQLAlchemy(app)
+# Init driver_db
+driver_db = SQLAlchemy(app)
+
 # Init ma
-ma = Marshmallow(app)
+driver_ma = Marshmallow(app)
 
-# User Class/Model
+#Init traffic_db
+traffic_db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    driver_id = db.Column(db.String(200), unique = True)
-    email = db.Column(db.String(200))
-    contact = db.Column(db.Integer, unique = True)
-    password = db.Column(db.String(200))
-    job = db.Column(db.String(200))
+# Init traffic_ma
+traffic_ma = Marshmallow(app)
 
 
-    def __init__(self, name, driver_id, email, contact, password, job):
+# Driver Class/Model
+class Driver(driver_db.Model):
+    did = driver_db.Column(driver_db.Integer, primary_key=True)
+    name = driver_db.Column(driver_db.String(100), unique=True)
+    driver_id = driver_db.Column(driver_db.String(200), unique = True)
+    email = driver_db.Column(driver_db.String(200))
+    contact = driver_db.Column(driver_db.Integer, unique = True)
+    password = driver_db.Column(driver_db.String(200))
+
+
+    def __init__(self, name, driver_id, email, contact, password):
         self.name = name
         self.driver_id = driver_id
         self.email = email
         self.contact = contact
         self.password = password
-        self.job = job
         
 
-# User Schema
-
-
-class UserSchema(ma.Schema):
+# Driver Schema
+class DriverSchema(driver_ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'driver_id', 'email', 'contact', 'password', 'job')
+        fields = ('id', 'name', 'driver_id', 'email', 'contact', 'password')
 
+
+# Traffic Class/Model
+class Traffic(traffic_db.Model):
+    tid = traffic_db.Column(traffic_db.Integer, primary_key=True)
+    name = traffic_db.Column(traffic_db.String(100), unique=True)
+    traffic_id = traffic_db.Column(traffic_db.String(200), unique = True)
+    email = traffic_db.Column(traffic_db.String(200))
+    contact = traffic_db.Column(traffic_db.Integer, unique = True)
+    password = traffic_db.Column(traffic_db.String(200))
+    
+
+    def __init__(self, name, traffic_id, email, contact, password):
+        self.name = name
+        self.traffic_id = traffic_id
+        self.email = email
+        self.contact = contact
+        self.password = password
+
+
+        
+
+# Traffic Schema
+
+
+class TrafficSchema(traffic_ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'traffic_id', 'email', 'contact', 'password')
 
 def token_required(f):
     @wraps(f)
@@ -76,48 +105,47 @@ def token_required(f):
     return decorated
 
 # Init schema
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
+driver_schema = DriverSchema()
+drivers_schema = DriverSchema(many=True)
 
 # Sign up
-@app.route('/signup', methods=['POST'])
-def Sign_up():
+@app.route('/driver_signup', methods=['POST'])
+def Sign_up_driver():
     name = request.json['name']
     driver_id = request.json[ 'driver_id']
     email = request.json['email']
     contact = request.json['contact']
     password = request.json['password']
-    job = request.json['job']
 
     hashed_password = generate_password_hash(password, method = 'sha256')
 
-    new_user = User(name, driver_id, email, contact, hashed_password, job)
+    new_driver = Driver(name, driver_id, email, contact, hashed_password)
 
-    db.session.add(new_user)
-    db.session.commit()
+    driver_db.session.add(new_driver)
+    driver_db.session.commit()
 
-    return user_schema.jsonify(new_user)
+    return driver_schema.jsonify(new_driver)
 
-# Get Users
-@app.route('/user', methods=['GET'])
-def get_users():
-    all_users = User.query.all()    
-    result = users_schema.dump(all_users) #array vayeko le
+# Get Drivers
+@app.route('/driver', methods=['GET'])
+def get_drivers():
+    all_drivers = Driver.query.all()    
+    result = drivers_schema.dump(all_drivers) #array vayeko le
     return jsonify(result)
 
-# Get single users 
-@app.route('/user/<id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-    return user_schema.jsonify(user)
+# Get single drivers 
+@app.route('/driver/<id>', methods=['GET'])
+def get_driver(id):
+    driver = Driver.query.get(id)
+    return driver_schema.jsonify(driver)
 
-# Update a User
-@app.route('/user/<id>', methods=['PUT'])
-def update_user(id):
-    user = User.query.get(id)
+# Update a Driver
+@app.route('/driver/<id>', methods=['PUT'])
+def update_driver(id):
+    driver = Driver.query.get(id)
 
-    if not user:
-        return jsonify({'message': 'no user found'})
+    if not driver:
+        return jsonify({'message': 'no driver found'})
 
 
     name = request.json['name']
@@ -125,48 +153,143 @@ def update_user(id):
     email = request.json['email']
     contact = request.json['contact']
     password = request.json['password']
-    job = request.json['job']
 
-    user.name = name
-    user.driver_id = driver_id
-    user.email = email
-    user.contact = contact
-    user.password = password
-    user.job = job
-    new_user = User(name, driver_id, email, contect, password, job)
+    driver.name = name
+    driver.driver_id = driver_id
+    driver.email = email
+    driver.contact = contact
+    driver.password = password
+    new_driver = Driver(name, driver_id, email, contect, password)
 
-    db.session.commit()
+    driver_db.session.commit()
 
-    return user_schema.jsonify(user)
+    return driver_schema.jsonify(driver)
 
-# Delete users 
-@app.route('/user/<id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get(id)
-    if not user:
-        return jsonify({'message': 'no user found'})
-    db.session.delete(user)
-    db.session.commit()
-    return user_schema.jsonify(user)
+# Delete drivers 
+@app.route('/driver/<id>', methods=['DELETE'])
+def delete_driver(id):
+    driver = Driver.query.get(id)
+    if not driver:
+        return jsonify({'message': 'no driver found'})
+    driver_db.session.delete(driver)
+    driver_db.session.commit()
+    return driver_schema.jsonify(driver)
 
-# login users http basic auth 
-@app.route('/login')
+# login DriverSchema http basic auth 
+@app.route('/driver_login', methods=['POST'])
 def login():
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
 
-    user = User.query.filter_by(name = auth.username).first()
+    driver = Driver.query.filter_by(name = auth.username).first()
 
-    if not user:
+    if not driver:
         return make_response('Could not verify2', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
 
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'id': user.id}, app.config['SECRET_KEY'])
+    if check_password_hash(driver.password, auth.password):
+        token = jwt.encode({'id': driver.did}, app.config['SECRET_KEY'])
         return jsonify({'token': token.decode('UTF-8')})
 
     return make_response('Could not verify3', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+
+
+
+
+
+
+
+
+
+# Init traffic schema
+traffic_schema = TrafficSchema()
+traffics_schema = TrafficSchema(many=True)
+
+# Traffic Sign up
+@app.route('/traffic_signup', methods=['POST'])
+def Sign_up_traffic():
+    name = request.json['name']
+    traffic_id = request.json['traffic_id']
+    email = request.json['email']
+    contact = request.json['contact']
+    password = request.json['password']
+    hashed_password = generate_password_hash(password, method = 'sha256')
+
+    new_traffic = Traffic(name, traffic_id, email, contact, hashed_password)
+    traffic_db.session.add(new_traffic)
+    traffic_db.session.commit()
+    return traffic_schema.jsonify(new_traffic)
+
+# Get Traffics
+@app.route('/traffic', methods=['GET'])
+def get_traffics():
+    all_traffics = Traffic.query.all()    
+    result = traffics_schema.dump(all_traffics) #array vayeko le
+    return jsonify(result)
+
+# Get single traffics 
+@app.route('/traffic/<id>', methods=['GET'])
+def get_traffic(id):
+    traffic = Traffic.query.get(id)
+    return traffic_schema.jsonify(traffic)
+
+# Update a Traffic
+@app.route('/traffic/<id>', methods=['PUT'])
+def update_traffic(id):
+    traffic = Traffic.query.get(id)
+
+    if not traffic:
+        return jsonify({'message': 'no traffic found'})
+
+
+    name = request.json['name']
+    traffic_id = request.json['traffic_id']
+    email = request.json['email']
+    contact = request.json['contact']
+    password = request.json['password']
+
+    traffic.name = name
+    traffic.traffic_id = traffic_id
+    traffic.email = email
+    traffic.contact = contact
+    traffic.password = password
+    new_traffic = Traffic(name, traffic_id, email, contect, password)
+
+    traffic_db.session.commit()
+
+    return traffic_schema.jsonify(traffic)
+
+# Delete traffics 
+@app.route('/traffic/<id>', methods=['DELETE'])
+def delete_traffic(id):
+    traffic = Traffic.query.get(id)
+    if not traffic:
+        return jsonify({'message': 'no traffic found'})
+    traffic_db.session.delete(traffic)
+    traffic_db.session.commit()
+    return traffic_schema.jsonify(traffic)
+
+# login TrafficSchema http basic auth 
+@app.route('/traffic_login', methods=['POST'])
+def login_traffic():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+
+    traffic = Traffic.query.filter_by(name = auth.username).first()
+
+    if not traffic:
+        return make_response('Could not verify2', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+
+    if check_password_hash(traffic.password, auth.password):
+        token = jwt.encode({'id': traffic.tid}, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify3', 401, {'WWW-Authenticate': 'Basic realm = "Login required!"'})
+
+
 
 # Runserver
 if __name__ == "__main__":
